a02f5998ce84efd81a308fd6ac7ee68bdd2c10f1
railsbeginner <bartosz.dyja@gmail.com>
2016-05-29 15:16:41 +0200

    Add meal model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ActiveRecord::Base
+  has_many :meals
+  
   def self.from_oauth(profile)
     user = User.where(email: profile['email']).first_or_create do |u|
       u.username = profile['first_name'] || profile['login']
