0fa8e5bfe5414b59903ae477a425c038a8b40a6a
railsbeginner <bartosz.dyja@gmail.com>
2016-05-30 12:14:44 +0200

    Add user authentication specs

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,7 +7,8 @@ class User < ActiveRecord::Base
     user = User.where(email: profile['email']).first_or_create do |u|
       u.username = profile['first_name'] || profile['login']
     end
-    payload = {id: user.id}
+    p payload = {id: user.id}
+
     JWT.encode payload, Rails.application.secrets.jwt_secret, 'HS256'
   end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -15,4 +15,24 @@ RSpec.describe User do
     it { is_expected.to have_many :meals }
     it { is_expected.to have_many :orders }
   end
+
+  describe 'Oauth authenticate' do
+    let(:facebook) { { 'email' => 'abc@pl.pl', 'first_name' => 'John' } }
+    before do
+      @token = User.from_oauth(facebook)
+    end
+
+    it 'assigns name from profile' do
+      expect(User.last.username).to eq 'John'
+    end
+
+    it 'assigns email from profile' do
+      expect(User.last.email).to eq 'abc@pl.pl'
+    end
+
+    it 'encode user id to access token' do
+      decoded_token = JWT.decode @token, Rails.application.secrets.jwt_secret, true, { :algorithm => 'HS256' }
+      expect(decoded_token[0]['id']).to eq User.last.id
+    end
+  end
 end
