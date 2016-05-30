ae1ac158e7d5315860122f9d5748516c2f6d3e69
railsbeginner <bartosz.dyja@gmail.com>
2016-05-30 09:30:41 +0200

    Remove unnecessary routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   namespace :api, defaults: { format: :json } do
-    resource :user
-    resources :orders
-    resources :meals
+    resource :user, only: :show
+    resources :orders, only: [:create, :show, :index, :update]
+    resources :meals, only: [:create, :show, :update, :index]
   end
   post 'auth/facebook', to: 'auth#facebook'
   post 'auth/github', to: 'auth#github'
